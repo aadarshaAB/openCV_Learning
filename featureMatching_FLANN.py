b255d7ef2b5627e24c0e17539cb6d51204afe156
aadarshaAB <aadarshabhattarai873@gmail.com>
2023-03-29 23:27:28 +0545

    match features between two images using FLANN based Matcher

---
+++ featureMatching_FLANN.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+
+img1=cv.imread("1Matching_BT.jpg",cv.IMREAD_GRAYSCALE)
+img2=cv.imread("2Matching_BT.jpg",cv.IMREAD_GRAYSCALE)
+
+sift=cv.SIFT_create()
+
+kp1,des1=sift.detectAndCompute(img1,None)
+kp2,des2 = sift.detectAndCompute(img2, None)
+
+FLANN_INDEX_KDTREE=1
+index_params=dict(algorithm=FLANN_INDEX_KDTREE,trees=5)
+search_params=dict(checks=50)
+
+flann=cv.FlannBasedMatcher(index_params,search_params)
+
+matches=flann.knnMatch(des1,des2,k=2)
+
+matchesMask=[[0,0]for i in range(len(matches))]
+
+for i,(m,n) in enumerate(matches):
+    if m.distance<0.7*n.distance:
+        matchesMask[i]=[1,0]
+draw_params=dict(matchColor=(0,225,0),
+    singlePointColor=(255,0,0),
+    matchesMask=matchesMask,
+    flags=cv.DrawMatchesFlags_DEFAULT)
+img3=cv.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
+plt.imshow(img3)
+plt.show()
